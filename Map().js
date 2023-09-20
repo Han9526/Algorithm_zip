4119468cef425fe06536754fa2fea99cc79f5ee2
한승준 <jk2tmdwns@naver.com>
2023-09-20 22:11:28 +0900

    day_001

---
+++ Map().js
@@ -0,0 +1,7 @@
+// array.map(callback함수(value)=>{return 계산식})
+
+function solution(numbers) {
+   return numbers.map((answer)=>{
+        return answer*2;
+    })
+}
---
+++ Reverse().js
@@ -0,0 +1,6 @@
+// 배열 뒤집기 reverse()
+function solution(num_list) {
+    num_list.reverse(); 
+    return num_list;
+}
+
