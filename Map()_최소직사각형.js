e13ce97d8958a733cdcedff33946ce4900425b4f
한승준 <jk2tmdwns@naver.com>
2023-09-21 18:21:18 +0900

    ver_002

---
+++ Map()_최소직사각형.js
@@ -0,0 +1,9 @@
+// 최소직사각형 찾기
+function solution(sizes) {
+    // 내림차순 배열을 생성
+    var sizes_desc = sizes.map(index => (index[0] > index[1] ? [index[0], index[1]] : [index[1], index[0]]));
+    //  최댓값 찾기
+    var firstMax = Math.max(...sizes_desc.map(f_index => f_index[0]));
+    var secMax = Math.max(...sizes_desc.map(s_index => s_index[1]));
+    var maxsizes = firstMax * secMax;
+    };
